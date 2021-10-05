7755d99dd8c5bef37a3270e051264f3b6037931f
gracetm <gracetongmin@gmail.com>
2021-10-05 23:37:27 +0800

    completed footRE

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,58 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { useState } from "react";
 
 export default function Footer() {
-  return <div className={styles.footerContainer}>This is the footer</div>;
+  const [show, setShow] = useState(true);
+  const onClickHandler = () => {
+    setShow((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.footerContainer}>
+      <div>
+        {show && (
+          // only if show is true then render component in brackets
+          <div className={styles.columnComps}>
+            <div className={styles.column}>
+              <text className={styles.title}>Social Media</text>
+              <text className={styles.body}>Instagram</text>
+              <text className={styles.body}>Facebook</text>
+              <text className={styles.body}>Twitter</text>
+            </div>
+
+            <div className={styles.column}>
+              <text className={styles.title}>About Us</text>
+              <text className={styles.body}>Mission Statement</text>
+              <text className={styles.body}>Vision</text>
+              <text className={styles.body}>Our Goals</text>
+            </div>
+
+            <div className={styles.column}>
+              <text className={styles.title}>Contact Us</text>
+              <text className={styles.body}>Live Chat</text>
+              <text className={styles.body}>Location</text>
+            </div>
+
+            <div className={styles.column}>
+              <text className={styles.title}>Quicklinks</text>
+              <text className={styles.body}>Reviews</text>
+              <text className={styles.body}>Recycle Now</text>
+              <text className={styles.body}>Directions</text>
+              <text className={styles.body}>Login</text>
+              <text className={styles.body}>Help</text>
+            </div>
+          </div>
+        )}
+
+        <text className={styles.terms}>
+          All rights reserved. Terms and Conditions apply.
+        </text>
+
+        <button className={styles.button} onClick={onClickHandler}>
+          {show == true ? "Hide" : "Show"}
+        </button>
+      </div>
+    </div>
+  );
 }
