2d6ceb8f6736c6f88bb13f4e1ad5ef9fb5dd127c
Jing Xuan <ongjx16@gmail.com>
2021-10-11 19:29:43 +0800

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,52 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
-  return <header className={styles.header}>This is the header</header>;
+  const [color, setColor] = useState("white");
+
+
+  return (
+    < div >
+      <header style={{ background: color, color: setColor }} className={styles.header} >
+
+        <div className={styles.title}>
+          Recycle Company
+        </div>
+        <div onClick={() => { setColor(prev => prev === "white" ? "lightgreen" : "white") }} className={styles.navigation}>
+          <text className={styles.navigationBody}>Home</text>
+          <text className={styles.navigationBody}>Profile</text>
+          <text className={styles.navigationBody}>Settings</text>
+          <text className={styles.navigationBody}>Change</text>
+        </div>
+
+
+      </header>;
+    </div >
+  )
 }
+
+
+// export default function Header() {
+//   const [color, setColor] = useState("white");
+
+
+//   return (
+//     < div >
+//       <header style={{ background: color, color: setColor }} className='header' >
+
+//         <div className={styles.title}>
+//           Recycle Company
+//         </div>
+//         <div onClick={() => { setColor("lightgreen") }} className={styles.navigation}>
+//           <text className={styles.navigationBody}>Home</text>
+//           <text className={styles.navigationBody}>Profile</text>
+//           <text className={styles.navigationBody}>Settings</text>
+//           <text className={styles.navigationBody}>Change</text>
+//         </div>
+
+
+//       </header>;
+//     </div >
+//   )
+// }
